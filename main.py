39949750b8fb2b12070563376886779cfa170ac5
StrangeSas <strange.sas@yandex.ru>
2024-01-23 15:58:45 +0700

    Saving stuff

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 import pandas as pd
 import yaml
+import json
+import re
 
 irisdf = pd.read_csv('files to parse/apple_quality.csv')
 
 irisdf = irisdf.loc[(irisdf.Quality == "good") & (irisdf.Sweetness >= 5.0)]
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+print(irisdf.describe())
+for index, row in irisdf.iterrows():
+    if row.Size >= -1.0:
+        print("Awesome apple")
 
-print(irisdf)
-with open('anaconda-project.yml', 'w') as file:
-    documents = yaml.dump(irisdf.to_dict(orient="records"), file, default_flow_style=False)
+irisdf["Total"] = irisdf["Sweetness"] + irisdf["Juiciness"] + irisdf["Ripeness"]\
+                  + irisdf["Crunchiness"]+ irisdf["Weight"] + irisdf["Size"]
+print(type(irisdf))
+
+irisdf = irisdf.loc[irisdf["Size"] >= -1.0, 'Juiciness'] = 5
+
+print(type(irisdf))
+
+irisdf.reset_index(drop=True, inplace=True)
+with open('anaconda-project.json', 'w') as file:
+    documents = json.dump(irisdf.to_dict(orient="records"), file, indent=2)
+
+# If you would need something to disassemble a text - you can use regex
