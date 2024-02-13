8e47a7b87e995d9faabb2652409b2b716def3a0b
StrangeSas <59920946+StrangeSas@users.noreply.github.com>
2024-02-13 16:01:06 +0700

    I want to hang myself :3

---
+++ modelTrain/zeml bullshit/.ipynb_checkpoints/model_train-checkpoint.py
@@ -0,0 +1,33 @@
+import sys
+
+sys.path.append('../../steps/')
+
+import pandas as pd
+import logging
+from zenml import step
+
+from src.model_dev import LinearRegressionModel
+from steps.config import ModelNameConfig
+from sklearn.base import RegressorMixin
+@step
+def train_model(
+        X_train: pd.DataFrame,
+        X_test: pd.DataFrame,
+        Y_train: pd.DataFrame,
+        Y_test: pd.DataFrame,
+        config: ModelNameConfig,
+) -> LinearRegressionModel:
+    """
+    Trains the model on ingested data
+    """
+    try:
+        model = None
+        if config.model_name == "LinearRegression":
+            model = LinearRegressionModel()
+            trained_model = model.train(X_train, Y_train)
+            return trained_model
+        else:
+            raise ValueError("Model {} not supported".format(config.model_name))
+    except Exception as e:
+        logging.error("Error in training model: {}".format(e))
+        raise e
